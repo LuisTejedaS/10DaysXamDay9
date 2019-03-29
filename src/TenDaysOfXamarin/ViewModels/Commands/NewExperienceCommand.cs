090476b4f4d71093fd42ea462c54f2f3ef6eba9f
Luis Tejeda <luis.tejeda@contpaqi.com>
2019-03-29 16:21:03 -0600

    solucion

---
+++ src/TenDaysOfXamarin/ViewModels/Commands/NewExperienceCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace TenDaysOfXamarin.ViewModels.Commands
+{
+    public class NewExperienceCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private ExperiencesVM viewModel;
+        public NewExperienceCommand(ExperiencesVM viewModel)
+        {
+            this.viewModel = viewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            viewModel.NewExperience();
+        }
+    }
+}
---
+++ src/TenDaysOfXamarin/ViewModels/ExperiencesVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using TenDaysOfXamarin.ViewModels.Commands;
+
+namespace TenDaysOfXamarin.ViewModels
+{
+    public class ExperiencesVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public NewExperienceCommand NewExperienceCommand { get; set; }
+
+        public void NewExperience()
+        {
+            App.Current.MainPage.Navigation.PushAsync(new MainPage());
+        }
+
+        public ExperiencesVM()
+        {
+            NewExperienceCommand = new NewExperienceCommand(this);
+        }
+    }
+}
